9be5d2ed6e52f5948be9ec6fa0cabeb68b9af070
RomanKononenko <roman.kononenko2018@gmail.com>
2024-04-22 10:33:22 +0300

    first

---
+++ w5500_spi/build/CMakeFiles/w5500_spi.dir/socket.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/w5500_spi.dir/socket.c.obj: \
+ C:\custom_ethernet_w5500\w5500_spi\socket.c \
+ C:\custom_ethernet_w5500\w5500_spi\socket.h \
+ C:\custom_ethernet_w5500\w5500_spi\wizchip_conf.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\stdint.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\machine\_default_types.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\sys\features.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\_newlib_version.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\sys\_intsup.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\sys\_stdint.h \
+ C:\custom_ethernet_w5500\w5500_spi\w5500.h
---
+++ w5500_spi/build/CMakeFiles/w5500_spi.dir/w5500.c.obj.d
@@ -0,0 +1,11 @@
+CMakeFiles/w5500_spi.dir/w5500.c.obj: \
+ C:\custom_ethernet_w5500\w5500_spi\w5500.c \
+ C:\custom_ethernet_w5500\w5500_spi\w5500.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\stdint.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\machine\_default_types.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\sys\features.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\_newlib_version.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\sys\_intsup.h \
+ c:\progra~1\raspbe~1\picosd~1.1\gcc-ar~1\arm-none-eabi\include\sys\_stdint.h \
+ C:\custom_ethernet_w5500\w5500_spi\wizchip_conf.h
